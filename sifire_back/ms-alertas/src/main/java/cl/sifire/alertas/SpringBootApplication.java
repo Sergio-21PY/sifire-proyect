700b0e00d937f14595267c77e7dad443ede81d0f
Matias Chavez <120347956+MatiDroid21@users.noreply.github.com>
2026-04-27 20:19:29 -0400

    a revisar conexion y eureka para pasar a master

---
+++ sifire_back/ms-alertas/src/main/java/cl/sifire/alertas/SpringBootApplication.java
@@ -0,0 +1,5 @@
+package cl.sifire.alertas;
+
+public @interface SpringBootApplication {
+
+}
---
+++ sifire_back/ms-usuarios/src/main/java/cl/duoc/ser/sotoc/sifirebackend/service/UsuarioService.java
@@ -0,0 +1,8 @@
+package cl.duoc.ser.sotoc.sifirebackend.service;
+
+import cl.duoc.ser.sotoc.sifirebackend.model.Usuario;
+import cl.duoc.ser.sotoc.sifirebackend.repository.UsuarioRepository;
+
+public interface UsuarioService extends UsuarioRepository {
+    
+}
