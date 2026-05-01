6b991b68ffeeab1d6c20f8d0ae487a428a20acbb
Matias Chavez <120347956+MatiDroid21@users.noreply.github.com>
2026-05-01 17:43:17 -0400

    fix: se corrigen los imports y anotaciones en el ms de monitoreo

--- sifire_back/ms-monitoreo/src/main/java/cl/sifire/monitoreo/model/ZonaRiesgo.java
+++ sifire_back/ms-monitoreo/src/main/java/cl/sifire/monitoreo/model/ZonaRiesgo.java
@@ -1,7 +1,6 @@
 package cl.sifire.monitoreo.model;
 
-import org.springframework.data.annotation.Id;
-
+import jakarta.persistence.Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
--- sifire_back/ms-monitoreo/src/main/java/cl/sifire/monitoreo/service/MonitoreoService.java
+++ sifire_back/ms-monitoreo/src/main/java/cl/sifire/monitoreo/service/MonitoreoService.java
@@ -3,6 +3,7 @@ package cl.sifire.monitoreo.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import cl.sifire.monitoreo.model.AsignacionBrigada;
 import cl.sifire.monitoreo.model.Brigada;
@@ -13,6 +14,7 @@ import cl.sifire.monitoreo.repository.BrigadaRepository;
 import cl.sifire.monitoreo.repository.RutaEvacuacionRepository;
 import cl.sifire.monitoreo.repository.ZonaRiesgoRepository;
 
+@Service
 public class MonitoreoService {
     private final ZonaRiesgoRepository zonaRiesgoRepository;
     private final RutaEvacuacionRepository rutaEvacuacionRepository;
